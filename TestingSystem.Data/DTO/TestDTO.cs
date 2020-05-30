ce6fcaad7f8c8049e593ff0cd6be9125a62dab99
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-05-30 20:49:22 +0300

    Add Connection, TestDTO, methods for Test

---
+++ TestingSystem.Data/DTO/TestDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Data.DTO
+{
+    public class TestDTO
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public TimeSpan duration { get; set; }
+        public int score { get; set; }
+
+        public TestDTO()
+        {
+        }
+
+        public TestDTO(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public TestDTO(int Id, string name, TimeSpan duration, int score)
+        {
+            this.Id = Id;
+            this.name = name;
+            this.duration = duration;
+            this.score = score;
+        }
+    }
+}
