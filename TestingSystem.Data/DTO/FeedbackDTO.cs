85c897e3e6c3ae593cde88f7771489a8ee4edf6e
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-05-30 23:25:37 +0300

    Add AttemptResultDBO and method for all User attempts by testID

---
+++ TestingSystem.Data/DTO/FeedbackDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Data.DTO
+{
+    public class FeedbackDTO
+    {
+        public int Id { get; set; }
+        public int userId { get; set; }
+        public int questionId { get; set; }
+        public string message { get; set; }
+        public DateTime date { get; set; }
+        public Boolean processed { get; set; }
+
+        public FeedbackDTO()
+        {
+        }
+
+        public FeedbackDTO(int userId, DateTime date)
+        {
+            this.userId = userId;
+            this.date = date;
+        }
+
+        public FeedbackDTO(int Id, int userId, int questionId, string message, DateTime date, Boolean processed)
+        {
+            this.Id = Id;
+            this.userId = userId;
+            this.questionId = questionId;
+            this.message = message;
+            this.date = date;
+            this.processed = processed;
+        }
+    }
+}
---
+++ TestingSystem.Data/DTO/FeedbackQuestionDBO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Data.DTO
+{
+    public class FeedbackQuestionDBO
+    {
+        
+        public string message { get; set; }
+        public string value { get; set; }
+
+        public FeedbackQuestionDBO()
+        {
+        }        
+    }
+}
