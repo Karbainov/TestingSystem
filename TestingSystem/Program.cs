a3facb8351c4eda588a4a5f4c1bdac30d24cf84e
El_makso <El_makso@mail.ru>
2020-05-30 16:27:52 +0300

    Start

---
+++ TestingSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
