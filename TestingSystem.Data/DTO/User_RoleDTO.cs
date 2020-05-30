ad0e673d902c2c6876fa2d771bd6d7274b8c39fa
serg <s.401n@mail.ru>
2020-05-30 17:14:17 +0300

    add Role and User_Role

---
+++ TestingSystem.Data/DTO/User_RoleDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Data.DTO
+{
+    class User_RoleDTO
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public int RoleID { get; set; }
+        public User_RoleDTO(int id, int userID, int roleID)
+        {
+            ID = id;
+            UserID = userID;
+            RoleID = roleID;
+        }
+        public User_RoleDTO()
+        {
+
+        }
+    }
+}
---
+++ TestingSystem.Data/StoredProcedure/CRUD/Role.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using TestingSystem.Data.DTO;
+
+namespace TestingSystem.Data.StoredProcedure.CRUD
+{
+    class Role
+    {
+        public int Role_Create(SqlConnection connection, RoleDTO role)
+        {
+            connection.Open();
+            string sqlExpression = "Role_Create";
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter nameParam = new SqlParameter("@Name", role.Name);
+            command.Parameters.Add(nameParam);
+            return command.ExecuteNonQuery();
+        }
+        public int Role_Delete(SqlConnection connection, RoleDTO role)
+        {
+            connection.Open();
+            string sqlExpression = "Role_Delete";
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter idParam = new SqlParameter("@ID", role.ID);
+            command.Parameters.Add(idParam);
+            return command.ExecuteNonQuery();
+        }
+        public int Role_Update(SqlConnection connection, RoleDTO role)
+        {
+            connection.Open();
+            string sqlExpression = "Role_Update";
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter idParam = new SqlParameter("@ID", role.ID);
+            command.Parameters.Add(idParam);
+            SqlParameter nameParam = new SqlParameter("@Name", role.Name);
+            command.Parameters.Add(nameParam);
+            return command.ExecuteNonQuery();
+        }
+        public List<RoleDTO> Role_Read(SqlConnection connection)
+        {
+            connection.Open();
+            string sqlExpression = "Role_Read";
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlDataReader reader = command.ExecuteReader();
+            List<RoleDTO> roles = new List<RoleDTO>();
+            if (reader.HasRows) // если есть данные
+            {
+
+                while (reader.Read()) // построчно считываем данные
+                {
+                    RoleDTO role = new RoleDTO();
+
+                    role.ID = (int)reader["id"];
+                    role.Name = (string)reader["Name"];
+                    roles.Add(role);
+                }
+            }
+            reader.Close();
+
+            return roles;
+        }
+        public List<RoleDTO> Role_ReadById(SqlConnection connection, RoleDTO role)
+        {
+            connection.Open();
+            string sqlExpression = "Role_ReadById";
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlParameter idParam = new SqlParameter("@ID", role.ID);
+            command.Parameters.Add(idParam);
+            SqlDataReader reader = command.ExecuteReader();
+            List<RoleDTO> roles = new List<RoleDTO>();
+            if (reader.HasRows) // если есть данные
+            {
+
+                while (reader.Read()) // построчно считываем данные
+                {
+                    RoleDTO newRole = new RoleDTO();
+
+                    //role.ID = (int)reader["ID"];
+                    role.Name = (string)reader["Name"];
+                    roles.Add(role);
+                }
+            }
+            reader.Close();
+
+            return roles;
+        }
+    }
+}
