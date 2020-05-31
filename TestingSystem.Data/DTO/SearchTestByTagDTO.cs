628a1f2118e8ffb4c2707e09e713ea624cc92cb0
Slava <zaharko@yandex.ru>
2020-05-31 11:05:05 +0300

    Search test by tag

---
+++ TestingSystem.Data/DTO/SearchTestByTagDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Data.DTO
+{
+    public class SearchTestByTagDTO
+    {
+        public string tag1 { get; set; }
+        public string tag2 { get; set; }
+        public string tag3 { get; set; }
+
+        public SearchTestByTagDTO() { }
+
+        public SearchTestByTagDTO(string tag1, string tag2, string tag3)
+        {
+            this.tag1 = tag1;
+            this.tag2 = tag2;
+            this.tag3 = tag3;
+        }
+
+    }
+}
