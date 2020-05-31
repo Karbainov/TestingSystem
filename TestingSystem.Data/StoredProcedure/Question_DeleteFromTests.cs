ff465dad00e8b59b070648f025093cf932987402
Slava <zaharko@yandex.ru>
2020-05-31 11:28:47 +0300

    delete question from test

---
+++ TestingSystem.Data/StoredProcedure/Question_DeleteFromTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using TestingSystem.Data.DTO;
+
+namespace TestingSystem.Data.StoredProcedure
+{
+    class Question_DeleteFromTests
+    {
+        static public int Question_DeleteFromTest(SqlConnection connection, Question_DeleteFromTestDTO question_Del_Test)
+        {
+            connection.Open();
+            string sqlExpression = "Test_Group_Add";
+
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter questionIdParam = new SqlParameter("@idGroup", question_Del_Test.questionId);
+            command.Parameters.Add(questionIdParam);
+
+            SqlParameter testIdParam = new SqlParameter("@idTest", question_Del_Test.testId);
+            command.Parameters.Add(testIdParam);
+
+            return command.ExecuteNonQuery();
+        }
+    }
+}
