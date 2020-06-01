c6a2514555cef3c41f0639965b2d62bc83d332f1
Vadim Vlasov <auxilant@gmail.com>
2020-06-01 06:43:22 +0300

    Add Tag and TestTag DTOs

---
+++ TestingSystem.Data/DTO/TestTagDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Data.DTO
+{
+    public class TestTagDTO
+    {
+        public int ID { get; set; }
+        public int TestID { get; set; }
+        public int TagID { get; set; }
+
+        public TestTagDTO() { }
+
+        public TestTagDTO(int id, int testId, int tagId)
+        {
+            ID = id;
+            TestID = testId;
+            TagID = tagId;
+        }
+    }
+}
