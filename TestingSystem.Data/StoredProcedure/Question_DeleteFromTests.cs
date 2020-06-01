4678d67580802ff669c6824434cebd1a5e495011
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-06-01 11:46:46 +0300

    Add constructor

--- TestingSystem.Data/StoredProcedure/Question_DeleteFromTests.cs
+++ TestingSystem.Data/StoredProcedure/Question_DeleteFromTests.cs
@@ -11,15 +11,15 @@ namespace TestingSystem.Data.StoredProcedure
         static public int Question_DeleteFromTest(SqlConnection connection, Question_DeleteFromTestDTO question_Del_Test)
         {
             connection.Open();
-            string sqlExpression = "Test_Group_Add";
+            string sqlExpression = "Question_DeleteFromTest";
 
             SqlCommand command = new SqlCommand(sqlExpression, connection);
             command.CommandType = System.Data.CommandType.StoredProcedure;
 
-            SqlParameter questionIdParam = new SqlParameter("@idGroup", question_Del_Test.questionId);
+            SqlParameter questionIdParam = new SqlParameter("@questionId", question_Del_Test.questionId);
             command.Parameters.Add(questionIdParam);
 
-            SqlParameter testIdParam = new SqlParameter("@idTest", question_Del_Test.testId);
+            SqlParameter testIdParam = new SqlParameter("@testId", question_Del_Test.testId);
             command.Parameters.Add(testIdParam);
 
             return command.ExecuteNonQuery();
