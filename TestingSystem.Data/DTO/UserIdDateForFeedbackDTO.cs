c75ec0d981d070c106edbc6457c2f3db2c2a7562
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-06-02 19:14:55 +0300

    Find Feedback and Question by UserId and Date with dapper

---
+++ TestingSystem.Data/DTO/UserIdDateForFeedbackDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Data.DTO
+{
+    public class UserIdDateForFeedbackDTO
+    {
+        public int UserID { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public UserIdDateForFeedbackDTO()
+        {
+        }
+
+        public UserIdDateForFeedbackDTO(int userId, DateTime date)
+        {
+            UserID = userId;
+            DateTime = date;
+        }
+    }
+}
