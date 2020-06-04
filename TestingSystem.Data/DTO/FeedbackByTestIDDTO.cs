e79487723210f19993a5539686f4450987014d95
Daria <baodaria@gmail.com>
2020-06-04 11:20:35 +0300

    added FeedbackByTestIDDTO + method

---
+++ TestingSystem.Data/DTO/FeedbackByTestIDDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Data.DTO
+{
+    class FeedbackByTestIDDTO
+    {
+        public int testID { get; set; }
+        public string message { get; set; }
+        public string value { get; set; }
+
+        public FeedbackByTestIDDTO()
+        {
+        }
+
+        public FeedbackByTestIDDTO(int TestID)
+        {
+            this.testID = testID;
+
+        }
+    
+
+    }
+}
