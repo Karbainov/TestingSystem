e804adfccb486cb8db43d688ed708fe77bf89504
serg <s.401n@mail.ru>
2020-06-05 14:19:52 +0300

    create UserGroupDTO and update UserManager

--- TestingSystem.Data/StoredProcedure/UserManager.cs
+++ TestingSystem.Data/StoredProcedure/UserManager.cs
@@ -12,38 +12,14 @@ namespace TestingSystem.Data.StoredProcedure
     public class UserManager
     {
 
-        public int AddStudentAndPutThemIntoGroup(SqlConnection connection, UserDTO user, GroupDTO group)//добавление студента сразу в группу
+        public int AddStudentAndPutThemIntoGroup(UserGroupDTO userGroup)//добавление студента сразу в группу
         {
-            connection.Open();
-            string sqlExpression = "User_Create";
-            SqlCommand command = new SqlCommand(sqlExpression, connection);
-            command.CommandType = System.Data.CommandType.StoredProcedure;
-
-            SqlParameter GroupParam = new SqlParameter("@GroupID", group.id);
-            command.Parameters.Add(GroupParam);
-
-            SqlParameter FirstNameParam = new SqlParameter("@FirstName", user.FirstName);
-            command.Parameters.Add(FirstNameParam);
-
-            SqlParameter LastNameParam = new SqlParameter("@LastName", user.LastName);
-            command.Parameters.Add(LastNameParam);
-
-            SqlParameter BDParam = new SqlParameter("@BirthDate", user.BirthDate);
-            command.Parameters.Add(BDParam);
-
-            SqlParameter LogParam = new SqlParameter("@Login", user.Login);
-            command.Parameters.Add(LogParam);
-
-            SqlParameter PassParam = new SqlParameter("@Password", user.Password);
-            command.Parameters.Add(PassParam);
-
-            SqlParameter EmailParam = new SqlParameter("@Email", user.Email);
-            command.Parameters.Add(EmailParam);
-
-            SqlParameter PhoneParam = new SqlParameter("@Phone", user.Phone);
-            command.Parameters.Add(PhoneParam);
-
-            return command.ExecuteNonQuery();
+            using(IDbConnection connection = Connection.GetConnection())
+            {
+                string sqlExpression = "User_Create @GroupID,@FirstName,@LastName,@BirthDate,@Login,@Password,@Email,@Phone";
+                return connection.Query<int>(sqlExpression, userGroup).FirstOrDefault();
+            }
+            
         }
         //public int User_DeleteAccount(SqlConnection connection, UserDTO user)//удаление студента и всего, что с ним связано
         //{
