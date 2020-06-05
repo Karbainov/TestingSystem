86a7bda9ae6bd64835b306cacb848405a9ba1ddf
serg <s.401n@mail.ru>
2020-06-05 14:11:36 +0300

    add some methods to StudentDataAccess

--- TestingSystem.Data/StudentDataAccess.cs
+++ TestingSystem.Data/StudentDataAccess.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Text;
 using TestingSystem.Data.DTO;
+using TestingSystem.Data.StoredProcedure;
+using TestingSystem.Data.StoredProcedure.CRUD;
 
 namespace TestingSystem.Data
 {
     public class StudentDataAccess
     {
+        public void CreateFeedback(FeedbackDTO feedbackDTO)
+        {
+            FeedbackCRUD feedback = new FeedbackCRUD();
+            feedback.FeedbackAdd(feedbackDTO);
+        }
+        public List<TestAttemptDTO> GetCompleteTest(int userID)
+        {
+            TestManager manager = new TestManager();
+            return manager.GetCompletedTestsByUserID(userID);
+        }
+        public List<TestAttemptDTO> GetIncompleteTest(int userID)
+        {
+            UserManager user = new UserManager();
+            return user.GetIncompleteTests(userID);
+        }
+        public List<AllStudentTestsDTO> GetAllStudentsTests(int userID)
+        {
+            UserManager user = new UserManager();
+            return user.GetStudentVsTests(userID);
+        }
         public void SelectActiveTest() 
         {
 
