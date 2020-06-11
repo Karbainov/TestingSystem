ddb4383f244a595c994c6d006e610acdc172a550
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-06-11 22:47:40 +0300

    Add models for Tests, TestId, QuestionId

---
+++ TestingSystem.API/Models/Input/QuestionIdInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingSystem.API.Models.Input
+{
+    public class QuestionIdInput
+    {        
+        public string QuestionValue { get; set; }
+        //?public byte AnswersCount { get; set; }
+        public byte Weight { get; set; }
+
+        public string TypeName { get; set; }
+        
+        public string AnswerValue { get; set; }
+        public bool Correct { get; set; }
+    }
+}
