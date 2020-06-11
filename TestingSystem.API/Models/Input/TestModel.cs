4884a6513f7e4995d6e0b8c1099326ed6a463f54
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-06-11 15:47:19 +0300

    Start do models

---
+++ TestingSystem.API/Models/Input/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingSystem.API.Models.Input
+{
+    public class TestModel
+    {
+        public string Name { get; set; }
+        public TimeSpan DurationTime { get; set; }        
+    }
+}
