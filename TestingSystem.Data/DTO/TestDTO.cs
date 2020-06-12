0d2e4850e21c26dc580f23eb2cd31b19ab2706db
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-06-13 00:40:27 +0300

    Change model and method for Test

--- TestingSystem.Data/DTO/TestDTO.cs
+++ TestingSystem.Data/DTO/TestDTO.cs
@@ -10,17 +10,19 @@ namespace TestingSystem.Data.DTO
         public string Name { get; set; }
         public TimeSpan DurationTime { get; set; }
         public int SuccessScore { get; set; }        
+        public int QuestionNumber { get; set; }        
 
         public TestDTO()
         {
         }
 
-        public TestDTO(int Id, string name, TimeSpan duration, int score)
+        public TestDTO(int Id, string name, TimeSpan duration, int score, int questionNumber)
         {
             ID = Id;
             Name = name;
             DurationTime = duration;
-            SuccessScore = score;            
+            SuccessScore = score;
+            QuestionNumber = questionNumber;
         }
     }
 }
