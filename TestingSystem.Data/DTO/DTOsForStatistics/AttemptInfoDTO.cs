f06372e9989b6254f25cb84fffa8d97f1f1245af
Vadim Vlasov <auxilant@gmail.com>
2020-06-18 17:55:38 +0300

    Info for statistics model draft

---
+++ TestingSystem.Data/DTO/DTOsForStatistics/AttemptInfoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Data.DTO.DTOsForStatistics
+{
+    public class AttemptInfoDTO
+    {
+        public int Number { get; set; }
+        public int UserId { get; set; }
+        public int TestId { get; set; }
+        public int UserResult { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Duration{ get; set; }
+    }
+}
