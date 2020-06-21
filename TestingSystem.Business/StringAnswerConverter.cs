fa8f3f660aa6ce4d098378998810533c237412d9
Vladislav <vladislovepanev@gmail.com>
2020-06-22 00:47:40 +0300

    Add remove white spaces method

--- TestingSystem.Business/StringAnswerConverter.cs
+++ TestingSystem.Business/StringAnswerConverter.cs
@@ -6,6 +6,15 @@ namespace TestingSystem.Business
 {
     public class StringAnswerConverter
     {
+        public string ConvertThirdType(string input)
+        {
+            
+            return input;
+        }
 
+        public static string RemoveWhitespace(string str)
+        {
+            return string.Join("", str.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
+        }
     }
 }
