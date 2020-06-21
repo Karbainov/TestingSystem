2e6af40e5788e3490537ca35f693d67e92854eb2
Vladislav <vladislovepanev@gmail.com>
2020-06-22 01:46:34 +0300

    Add check correct ans method

--- TestingSystem.Business/StringAnswerConverter.cs
+++ TestingSystem.Business/StringAnswerConverter.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace TestingSystem.Business
 {
     public class StringAnswerConverter
     {
+        public bool CheckCorrectAnswer(string answer, string correctAnswer)
+        {
+            if (answer.Length == correctAnswer.Length)
+            {
+                char[] answerArr = answer.ToArray();
+                char[] corrAnsArr = correctAnswer.ToArray();
+
+                for (int i = 0; i < corrAnsArr.Length; i++)
+                {
+                    if (corrAnsArr[i] != answerArr[i])
+                        return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
         public string ConvertThirdType(string input)
         {
             input = RemoveWhitespace(input);
