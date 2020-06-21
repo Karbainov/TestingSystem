2c5f3395fcec712ef368145281b3b83b1a9076d7
Slava <zaharko@yandex.ru>
2020-06-21 20:30:15 +0300

    fix

--- TestingSystem.Business/Statistics/Models/InfoModels/GroupInfoModel.cs
+++ TestingSystem.Business/Statistics/Models/InfoModels/GroupInfoModel.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace TestingSystem.Business.Statistics.Models.InfoModels
 {
-    class GroupInfoModel
+    public class GroupInfoModel
     {
+        public List<int> TestsId { get; set; }
+        public List<int> StudentsId { get; set; }
     }
 }
