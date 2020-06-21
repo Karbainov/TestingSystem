2bd2a1a538b6f8e0c213f7f459f76ff43f06168a
Alexander <sashakud0598@yandex.ru>
2020-06-21 21:31:24 +0300

    fix

--- TestingSystem.Business.Tests/ModelsForTests/PassedFailTest.cs
+++ TestingSystem.Business.Tests/ModelsForTests/PassedFailTest.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TestingSystem.Business.Statistics.Models;
 
 namespace TestingSystem.Business.Tests.ModelsForTests
 {
-    class PassedFailModelTest
+    public class PassedFailTest
     {
+        public PassedFailTest()
+        {
+
+        }
+           
+        public PassedFailedModel GetPassedFailed(int id)
+        {
+            PassedFailedModel pfm = new PassedFailedModel();
+        switch(id)
+        {
+            case 1:
+                    pfm.Passed = 4;
+                    pfm.Failed = 5;
+                    pfm.SuccessRate = 0.4;
+                    return pfm;
+            case 5:
+
+                    pfm.Passed = 0;
+                    pfm.Failed = 0;
+                    pfm.SuccessRate = 0;
+                    return pfm;
+            case 3:
+
+                    pfm.Passed = 0;
+                    pfm.Failed = 0;
+                    pfm.SuccessRate = 0;
+                    return pfm;
+        }
+            pfm.Passed = -1;
+            pfm.Failed = -1;
+            pfm.SuccessRate = -1;
+            return pfm;
+        }
     }
 }
