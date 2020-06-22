4ffde0d86914fdc30be833592c0a93b0a3a9d7c7
Vladislav <vladislovepanev@gmail.com>
2020-06-22 13:22:44 +0300

    Add remove non letter chars test

---
+++ TestingSystem.Business.Tests/StringAnswerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Business.Tests
+{
+    public class StringAnswerTests
+    {
+        [TestCase ("Hello", ExpectedResult = "Hello")]
+        [TestCase ("Ghbdtn ghbdtn привет", ExpectedResult = "Ghbdtn ghbdtn привет")]
+        [TestCase ("", ExpectedResult = "")]
+        [TestCase (" ", ExpectedResult = " ")]
+        [TestCase ("Hel(l)o'", ExpectedResult = "Hello")]
+        [TestCase ("П;р'и%&*в#е@т", ExpectedResult = "Привет")]
+        public string RemoveNonLetterCharsTest(string s)
+        {
+            StringAnswerConverter stringAnswerConverter = new StringAnswerConverter();
+
+            return stringAnswerConverter.RemoveNonLetterChars(s);
+        }
+    }
+}
