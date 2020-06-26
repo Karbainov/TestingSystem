add2494aabc2d7586c808b51f422734262f8edd6
Veronika Belova <belovaveronika14@gmail.com>
2020-06-26 13:35:51 +0300

    Create TeacherGroupsWithStudentsDTO

---
+++ TestingSystem.Data/DTO/TeacherGroupsWithStudentsDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingSystem.Data.DTO
+{
+    public class TeacherGroupsWithStudentsDTO
+    {
+        public int GroupID { get; set; }
+        public int GroupName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string StudentEmail { get; set; }
+        public string StudentPhone { get; set; }
+
+    }
+}
